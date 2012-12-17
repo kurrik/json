ebaee71aa80980a5783887eed81721fb0fa958c4
Arne Roomann-Kurrik <kurrik@twitter.com>
2012-12-17 15:02:56 -0800

    Fix docs

--- json.go
+++ json.go
@@ -14,14 +14,6 @@
 
 package json
 
-// This file provides a custom JSON parser suitable for processing Twitter data.
-// Some differences from the standard Golang json package:
-//   * Does not use reflection, parses into standard map/slice/value structs.
-//   * Parses numbers into int64 where possible, float64 otherwise.
-//   * Faster!  Probably due to no reflection:
-//      BenchmarkParseTweet	   10000	    155714 ns/op
-//      BenchmarkCustomJSON	   20000	     85822 ns/op
-
 import (
 	"bytes"
 	"fmt"
